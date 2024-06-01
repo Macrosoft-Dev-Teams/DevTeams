d922512da43268290d924891cb3cae9f997cd58a
Siyabonga Nkuna <sudosprogrammer@gmail.com>
2024-06-01 23:49:12 +0200

    Feature/app module (#14)
    * add app module config
    
    * update flyway script

---
+++ frontend/src/app/pages/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrl: './login.component.css',
+})
+export class LoginComponent {
+	formFields = {
+		signUp: {
+			name: {
+				order: 1,
+			},
+			email: {
+				order: 2,
+			},
+			password: {
+				order: 5,
+			},
+			confirm_password: {
+				order: 6,
+			},
+		},
+	};
+}
