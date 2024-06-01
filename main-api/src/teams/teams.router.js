a0f8fad1902a0ee8e2f61b7102aefc96af971b9d
shane-theron-bbd <159043311+shane-theron-bbd@users.noreply.github.com>
2024-06-01 23:33:38 +0200

    Edit team name added (#12)

--- main-api/src/teams/teams.router.js
+++ main-api/src/teams/teams.router.js
@@ -1,13 +1,24 @@
 const { Router } = require('express');
 const { withTransaction } = require('../db');
-const { createTeam } = require('./teams.db');
+const { createTeam, updateTeamName } = require('./teams.db');
 
 const teamsRouter = Router();
 
 teamsRouter.post('/', (req, res) => {
 	return withTransaction(async (tx) => {
-		const teamId = await createTeam(tx, req.body.teamName, res.locals.userId);
+		const teamId = await createTeam(tx, req.body.teamName, 1);
 		res.status(201).json({ teamId });
+	});
+});
+
+teamsRouter.put('/', (req, res) => {
+	return withTransaction(async (tx) => {
+		const teamName = await updateTeamName(
+			tx,
+			req.body.teamName,
+			req.body.teamId,
+		);
+		res.status(201).json({ teamName });
 	});
 });
 
