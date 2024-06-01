fd39e668e0cb721c5ef06093c8172b6c7367a4c3
Siya Nkuna <sudosprogrammer@gmail.com>
2024-06-02 00:57:38 +0200

    DT-12 code sanitization

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 
 import { Amplify } from 'aws-amplify';
-import { AmplifyAuthenticatorModule } from "@aws-amplify/ui-angular";
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from "./pages/login/login.component";
+import { LoginComponent } from './pages/login/login.component';
 
 import { environment } from '@src/environments/environment';
 
@@ -23,7 +23,12 @@ Amplify.configure({
 
 @NgModule({
 	declarations: [AppComponent, LoginComponent],
-	imports: [RouterOutlet, BrowserModule, AppRoutingModule, AmplifyAuthenticatorModule],
+	imports: [
+		RouterOutlet,
+		BrowserModule,
+		AppRoutingModule,
+		AmplifyAuthenticatorModule,
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
