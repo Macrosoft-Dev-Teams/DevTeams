3b0ada24332ae3e162cf613586409f4d8109fcce
Batandwa Mgutsi <Batandwa.Mgutsi@bbd.co.za>
2024-06-04 22:22:57 +0200

    Chats Controller view

--- frontend/src/app/components/chat-list/chat-list.component.ts
+++ frontend/src/app/components/chat-list/chat-list.component.ts
@@ -1,26 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Chat } from '@src/app/interfaces';
 import { ApiService } from '@src/app/services/api.service';
-import { BehaviorSubject, map } from 'rxjs';
+import { BehaviorSubject, Observable, map } from 'rxjs';
 
 @Component({
-  selector: 'app-chat-list',
-  templateUrl: './chat-list.component.html',
-  styleUrl: './chat-list.component.css'
+	selector: 'app-chat-list',
+	templateUrl: './chat-list.component.html',
+	styleUrl: './chat-list.component.css',
 })
 export class ChatListComponent {
-  chats = new BehaviorSubject<Chat[]>([]);
+	@Input() searchText?: Observable<string>;
+	chats = new BehaviorSubject<Chat[]>([]);
 
-  constructor(private apiService: ApiService) {}
+	constructor(private apiService: ApiService) {}
 
-  ngOnInit() {
-		this.apiService
-			.listChats()
-			.subscribe({
-				next: (chats) => this.chats.next(chats),
-				error: (error) => {
-					alert(error);
-				},
-			});
+	ngOnInit() {
+		this.apiService.listChats().subscribe({
+			next: (chats) => this.chats.next(chats),
+			error: (error) => {
+				alert(error);
+			},
+		});
+
+		this.searchText?.subscribe({
+			next: (text) => console.log(text),
+		});
 	}
 }
