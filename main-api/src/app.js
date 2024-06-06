fecdf29991ddcf8231d2d52c9b82d6d503aadfb1
Farisani-Ratshikombo-BBD <farisanipaul.ratshikombo@vcontractor.co.za>
2024-06-06 12:31:45 +0200

    Updating the app.js file

--- main-api/src/app.js
+++ main-api/src/app.js
@@ -16,23 +16,19 @@ const app = express();
 const helmet = require('helmet');
 const cors = require('cors');
 
-app.use(express.static(path.join(__dirname, 'dist/frontend/browser')));
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
 
+const authenticatedApiRouter = express.Router();
+authenticatedApiRouter.use(auth);
+authenticatedApiRouter.use('/teams', teamsRouter);
+authenticatedApiRouter.use('/chats', chatsRouter);
+authenticatedApiRouter.use('/users', usersRouter);
+
+app.use("/api", authenticatedApiRouter);
 app.use('/config', configRouter);
-
-app.use(auth);
-app.use('/teams', teamsRouter);
-app.use('/chats', chatsRouter);
-app.use('/users', usersRouter);
-
-app.get('*', (req, res) => {
-	res.sendFile('index.html', {
-		root: path.join(__dirname, 'dist/frontend/browser'),
-	});
-});
+app.use(express.static(path.join(__dirname, '../dist/frontend/browser')));
 
 app.listen(PORT, () => {
 	// eslint-disable-next-line no-console
