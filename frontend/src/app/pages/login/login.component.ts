825b84e256dc32a5f7953260e7280e1c78edc518
Siya Nkuna <sudosprogrammer@gmail.com>
2024-06-06 06:20:32 +0200

    DT-98 add user on sign up

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,8 +1,12 @@
+import { EventEmitter, Output } from '@angular/core';
 import { Component } from '@angular/core';
 import { AuthenticatorService } from '@aws-amplify/ui-angular';
 import { AuthService } from '@src/app/services/auth.service';
 
+import { ToastrService } from 'ngx-toastr';
+
 import { Hub } from 'aws-amplify/utils';
+import { ApiService } from '@src/app/services/api.service';
 
 @Component({
 	selector: 'app-login',
@@ -10,20 +14,36 @@ import { Hub } from 'aws-amplify/utils';
 	styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+	@Output() onUserAdd = new EventEmitter<number>();
+
 	constructor(
 		public authenticator: AuthenticatorService,
 		private authService: AuthService,
+		private apiService: ApiService,
+		private toastr: ToastrService,
 	) {
 		Hub.listen('auth', (data) => {
+			console.log('event type:', data.payload.event);
+
 			if (data.payload.event === 'signedIn') {
 				this.authService.handleAuthenticatedUser();
+				this.addUser();
 			}
 		});
 	}
 
 	ngOnInit() {
-    this.authService.handleAuthenticatedUser();
-  }
+		this.authService.handleAuthenticatedUser();
+	}
+
+	addUser() {
+		this.apiService.addUser().subscribe({
+			next: (userId) => this.onUserAdd.emit(userId),
+			error: (error) => {
+				this.toastr.error(error, 'Error!');
+			},
+		});
+	}
 
 	formFields = {
 		signUp: {
